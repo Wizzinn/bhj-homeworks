5f82762b137ea90ecc8f514116f3fc7198f3a099
N <weitch-96-96-96@mail.ru>
2023-08-23 22:49:47 +0300

    Задание 3 готово

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,31 @@
+(function() {
+    const tooltips = Array.from(document.querySelectorAll(`.has-tooltip`));
+    tooltips.forEach((button, index) => {
+      let attr = button.getAttribute(`title`);
+      let posTooltipTop = button.getBoundingClientRect().top;
+      let posTooltipLeft = button.getBoundingClientRect().left;
+      let hint = creatHint(posTooltipTop, posTooltipLeft, attr);
+      button.insertAdjacentHTML(`afterEnd`, hint.outerHTML);
+      button.addEventListener(`click`, (elem => {
+        elem.preventDefault();
+        showHint(button, index);
+      }));
+    })
+  }())
+  
+  function creatHint(posTop, posLeft, attribute) {
+    let hint = document.createElement(`div`);
+    hint.setAttribute(`class`, `tooltip`);
+    hint.setAttribute(`style`, `top: ${posTop + 20}px; left: ${posLeft}px`);
+    hint.textContent = attribute;
+    return hint;
+  }
+  
+  function showHint(tooltip, index) {
+    const hints = Array.from(tooltip.closest(`body`).querySelectorAll(`.tooltip`));
+    let currentPosition = hints.findIndex(item => item.className.includes(`tooltip_active`));
+    if (currentPosition !== -1 && currentPosition !== index) {
+      hints[currentPosition].classList.remove(`tooltip_active`);
+    }
+    hints[index].classList.toggle(`tooltip_active`);
+  }
